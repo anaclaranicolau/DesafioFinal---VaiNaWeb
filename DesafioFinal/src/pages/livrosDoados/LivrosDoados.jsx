39d15a736edd59451329a82af5f4a0595d9c122e
anaclaranicolau <nicolauaclara@gmail.com>
2025-03-31 19:36:13 -0300

    conexão back end

--- DesafioFinal/src/pages/livrosDoados/LivrosDoados.jsx
+++ DesafioFinal/src/pages/livrosDoados/LivrosDoados.jsx
@@ -1,18 +1,31 @@
 import Livro1 from '../../assets/livro1.png'
 import s from './livrosDoados.module.scss'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 export default function livrosDoados(){
+    
+    const [livros, setLivros] = useState([])
+
+    const puxarLivros = async() =>{
+        const resposta = await axios.get("https://desafio2vainaweb-api-livros.onrender.com/")
+        setLivros(resposta.data.livros)
+    }
+    useEffect(()=>{
+        puxarLivros()
+    }, [])
+    
     return(
         <section className={s.livrosDoadosSection}  >  
             <h2>Livros Doados</h2>
             <section className={s.containerCards} >
                <section>
-               <img src={Livro1} alt="Imagem da capa do livro O Porotagonista" />
-               <div>
-               <h3>O Protagonista</h3> 
-               <p>Susanne Andrade</p>
-               <p>Ficção</p>
-               </div>
+                <img src={Livro1} alt="Imagem da capa do livro O Porotagonista" />
+                    <div>
+                        <h3>O Protagonista</h3> 
+                        <p>Susanne Andrade</p>
+                        <p>Ficção</p>
+                    </div>
                </section>
             </section>
         </section>
--- DesafioFinal/src/pages/queroDoar/QueroDoar.jsx
+++ DesafioFinal/src/pages/queroDoar/QueroDoar.jsx
@@ -1,20 +1,57 @@
 import FormLivro from '../../assets/FormLivro.png'
 import s from './queroDoar.module.scss'
+import {useState} from 'react'
 
 export default function queroDoar(){
+    
+    const [titulo,setTitulo] = useState("")
+    const [categoria,setCategoria] = useState("")
+    const [autor,setAutor] = useState("")
+    const [image_url,setImage_url] = useState("")
+    
+    const capturaTitulo = (e) => {
+        setTitulo(e.target.value)
+    }
+
+    const capturaCategoria = (e) =>{
+        setCategoria(e.target.value)
+    }
+
+    const capturaAutor = (e) =>{
+        setAutor(e.target.value)
+    }
+
+    const capturaImg = (e) =>{
+        setImage_url(e.target.value)
+    }
+
+    const envioDados = async() => {
+
+        const dadosPEnviar = {
+            titulo,
+            categoria,
+            autor,
+            image_url
+        }
+        await axios.post("https://desafio2vainaweb-api-livros.onrender.com//", dadosPEnviar)
+    
+    }
+
+
+
     return(
         <section className={s.queroDoarSection} >
             <p>Por favor, preencha o formulário com suas informações e as informações do Livro</p>
-            <form action="">
+            <form onSubmit={(e)=> e.preventDefault()}>
                 <div>
                     <img src={FormLivro} alt="imagem de ícone livro aberto com borda azul" />
                     <h2>Informações do Livro</h2>
                 </div>
-                <input type="text" name="" id=""  placeholder='Título'/>
-                <input type="text" name="" id="" placeholder='Categoria'/>
-                <input type="text" name="" id="" placeholder='Autor'/>
-                <input type="text" name="" id="" placeholder='Link da Imagem'/>
-                <input type="submit" value="Doar"  className={s.buttonDoar} />
+                <input type="text" name="" id=""  placeholder='Título' onChange={capturaTitulo}/>
+                <input type="text" name="" id="" placeholder='Categoria' onChange={capturaCategoria} />
+                <input type="text" name="" id="" placeholder='Autor' onChange={capturaAutor}/>
+                <input type="url" name="" id="" placeholder='Link da Imagem' onChange={capturaImg} />
+                <input type="submit" value="Doar"  className={s.buttonDoar} onClick={envioDados} />
             </form>
         </section>
     )
